aa053504885e8862fe3ee244f524e293e51ebb97
jasonm066 <matjas432@gmail.com>
2026-08-17 23:21:30 +0100

    Make font declarations portable

--- src/gfx_d3d/r_font.h
+++ src/gfx_d3d/r_font.h
@@ -1,4 +1,5 @@
 #pragma once
+#include <qcommon/system.h>
 #include <cstdint>
 //#include "r_material.h"
 
@@ -28,23 +29,23 @@ struct Font_s // sizeof=0x18 // (SP/MP same)
 };
 static_assert(sizeof(void *) != 4u || sizeof(Font_s) == 24u);
 
-const Glyph *__cdecl R_GetCharacterGlyph(Font_s *font, uint32_t letter);
-uint32_t __cdecl R_FontGetRandomLetter(Font_s *font, int seed);
-void __cdecl TRACK_r_font();
-Font_s *__cdecl R_RegisterFont(const char *name, int imageTrack);
-Font_s *__cdecl R_RegisterFont_FastFile(const char *fontName);
-Font_s *__cdecl R_RegisterFont_LoadObj(const char *fontName, int imageTrack);
-Font_s *__cdecl R_LoadFont(const char *fontName, int imageTrack);
-double __cdecl R_NormalizedTextScale(Font_s *font, float scale);
-int __cdecl R_LetterWidth(uint32_t letter, Font_s *font);
-int __cdecl R_TextWidth(const char *text, int maxChars, Font_s *font);
-int __cdecl R_TextHeight(Font_s *font);
-const char *__cdecl R_TextLineWrapPosition(
+const Glyph *KISAK_CDECL R_GetCharacterGlyph(Font_s *font, uint32_t letter);
+uint32_t KISAK_CDECL R_FontGetRandomLetter(Font_s *font, int seed);
+void KISAK_CDECL TRACK_r_font();
+Font_s *KISAK_CDECL R_RegisterFont(const char *name, int imageTrack);
+Font_s *KISAK_CDECL R_RegisterFont_FastFile(const char *fontName);
+Font_s *KISAK_CDECL R_RegisterFont_LoadObj(const char *fontName, int imageTrack);
+Font_s *KISAK_CDECL R_LoadFont(const char *fontName, int imageTrack);
+double KISAK_CDECL R_NormalizedTextScale(Font_s *font, float scale);
+int KISAK_CDECL R_LetterWidth(uint32_t letter, Font_s *font);
+int KISAK_CDECL R_TextWidth(const char *text, int maxChars, Font_s *font);
+int KISAK_CDECL R_TextHeight(Font_s *font);
+const char *KISAK_CDECL R_TextLineWrapPosition(
     const char *text,
     int bufferSize,
     int pixelsAvailable,
     Font_s *font,
     float scale);
-int __cdecl R_ConsoleTextWidth(const char *textPool, int poolSize, int firstChar, int charCount, Font_s *font);
-void __cdecl R_InitFonts();
-void __cdecl R_ShutdownFonts();
+int KISAK_CDECL R_ConsoleTextWidth(const char *textPool, int poolSize, int firstChar, int charCount, Font_s *font);
+void KISAK_CDECL R_InitFonts();
+void KISAK_CDECL R_ShutdownFonts();
